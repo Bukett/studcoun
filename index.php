ab9da207041afed2ee50631873d8e799dd75a255
mac04 <mac04@193.140.5.173>
2014-02-08 13:05:38 +0200

    proje olusturuldu

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+    <title>StudCoun Main</title>
+</head>
+<body>
+<div id="find_buddy">
+    <h2>Find a buddy</h2>
+    <div>
+    <a href="findabuddy.php">Click here to find</a>
+    </div>
+</div>
+
+<div>
+    <h2>Want to be volunteer</h2>
+    <div>
+    <a href="volunteer_application.php">Click here volunteer</a>
+    </div>
+</div>
+
+<div id="universities">
+    <h2>Universities</h2>
+    <div id="top_universities">
+        <u1>
+            <li>Birinci üniversite</li>
+        </u1>
+        <div>
+        <a href="universities.php">Tüm Üniversiteler</a>
+        </div>
+    </div>
+</div>
+
+<div>
+    <h2>News § announcements</h2>
+    <div id="news_content">
+        haber içeriği burada olacak.
+    </div>
+    <div>
+        <a href="news.php">Tüm Haberler</a>
+    </div>
+</div>
+
+<div id="activities">
+    <h2>Activities</h2>
+    <div id="lastes_activities">
+        <u1>
+            <li>Birinci aktivites</li>
+        </u1>
+        <div>
+        <a href="activities.php">Tüm Haberler</a>
+        </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ volunteer_application.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+    <title>Voluntter App</title>
+</head>
+<body>
+    Eğer üye girişi yapılmış durumdaysa, member modülü içinde üye alanına yönlendirilecek.
+    <br>
+    aksi halde burada üyelik formu ve üye giriş formu gösterilecek.
+</body>
+</html>
